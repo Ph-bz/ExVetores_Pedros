ec05372f0cf554bd66085d2b7ff6f60f1876cb7a
Pedro <pedrolindono@gmail.com>
2025-12-03 22:17:08 -0300

    1- Criando e subindo o projeto de vetores

---
+++ Vetor_ex/Form2.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Vetor_ex
+{
+    partial class frmex1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblnum = new Label();
+            txtnum = new TextBox();
+            btnadd = new Button();
+            txtA = new TextBox();
+            txtB = new TextBox();
+            lblA = new Label();
+            lblB = new Label();
+            btnvoltar = new Button();
+            btnmostrar = new Button();
+            btnreset = new Button();
+            txtaddnum = new TextBox();
+            lbladdnum = new Label();
+            SuspendLayout();
+            // 
+            // lblnum
+            // 
+            lblnum.AutoSize = true;
+            lblnum.Location = new Point(24, 41);
+            lblnum.Name = "lblnum";
+            lblnum.Size = new Size(0, 25);
+            lblnum.TabIndex = 4;
+            // 
+            // txtnum
+            // 
+            txtnum.Location = new Point(24, 76);
+            txtnum.Name = "txtnum";
+            txtnum.Size = new Size(155, 32);
+            txtnum.TabIndex = 0;
+            // 
+            // btnadd
+            // 
+            btnadd.Location = new Point(24, 127);
+            btnadd.Name = "btnadd";
+            btnadd.Size = new Size(155, 31);
+            btnadd.TabIndex = 1;
+            btnadd.Text = "Adicionar Número";
+            btnadd.UseVisualStyleBackColor = true;
+            btnadd.Click += btnadd_Click;
+            // 
+            // txtA
+            // 
+            txtA.Location = new Point(249, 57);
+            txtA.Multiline = true;
+            txtA.Name = "txtA";
+            txtA.ReadOnly = true;
+            txtA.ScrollBars = ScrollBars.Both;
+            txtA.Size = new Size(88, 157);
+            txtA.TabIndex = 2;
+            // 
+            // txtB
+            // 
+            txtB.Location = new Point(364, 57);
+            txtB.Multiline = true;
+            txtB.Name = "txtB";
+            txtB.ReadOnly = true;
+            txtB.ScrollBars = ScrollBars.Both;
+            txtB.Size = new Size(88, 157);
+            txtB.TabIndex = 3;
+            // 
+            // lblA
+            // 
+            lblA.AutoSize = true;
+            lblA.Location = new Point(266, 34);
+            lblA.Name = "lblA";
+            lblA.Size = new Size(71, 25);
+            lblA.TabIndex = 5;
+            lblA.Text = "Lista A:";
+            // 
+            // lblB
+            // 
+            lblB.AutoSize = true;
+            lblB.Location = new Point(385, 34);
+            lblB.Name = "lblB";
+            lblB.Size = new Size(70, 25);
+            lblB.TabIndex = 6;
+            lblB.Text = "Lista B:";
+            // 
+            // btnvoltar
+            // 
+            btnvoltar.Location = new Point(419, 356);
+            btnvoltar.Name = "btnvoltar";
+            btnvoltar.Size = new Size(91, 46);
+            btnvoltar.TabIndex = 7;
+            btnvoltar.Text = "Voltar";
+            btnvoltar.UseVisualStyleBackColor = true;
+            btnvoltar.Click += btnvoltar_Click;
+            // 
+            // btnmostrar
+            // 
+            btnmostrar.Enabled = false;
+            btnmostrar.Location = new Point(24, 183);
+            btnmostrar.Name = "btnmostrar";
+            btnmostrar.Size = new Size(155, 31);
+            btnmostrar.TabIndex = 8;
+            btnmostrar.Text = "Mostrar Listas";
+            btnmostrar.UseVisualStyleBackColor = true;
+            btnmostrar.Click += btnmostrar_Click;
+            // 
+            // btnreset
+            // 
+            btnreset.Location = new Point(315, 356);
+            btnreset.Name = "btnreset";
+            btnreset.Size = new Size(91, 46);
+            btnreset.TabIndex = 9;
+            btnreset.Text = "Resetar";
+            btnreset.UseVisualStyleBackColor = true;
+            btnreset.Click += btnreset_Click;
+            // 
+            // txtaddnum
+            // 
+            txtaddnum.Location = new Point(24, 364);
+            txtaddnum.Name = "txtaddnum";
+            txtaddnum.ReadOnly = true;
+            txtaddnum.Size = new Size(100, 32);
+            txtaddnum.TabIndex = 32;
+            // 
+            // lbladdnum
+            // 
+            lbladdnum.AutoSize = true;
+            lbladdnum.Location = new Point(24, 332);
+            lbladdnum.Name = "lbladdnum";
+            lbladdnum.Size = new Size(202, 25);
+            lbladdnum.TabIndex = 33;
+            lbladdnum.Text = "Números Adicionados:";
+            // 
+            // frmex1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(708, 474);
+            ControlBox = false;
+            Controls.Add(lbladdnum);
+            Controls.Add(txtaddnum);
+            Controls.Add(btnreset);
+            Controls.Add(btnmostrar);
+            Controls.Add(btnvoltar);
+            Controls.Add(lblB);
+            Controls.Add(lblA);
+            Controls.Add(lblnum);
+            Controls.Add(txtB);
+            Controls.Add(txtA);
+            Controls.Add(btnadd);
+            Controls.Add(txtnum);
+            Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "frmex1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Exercício 1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label lblnum;
+        private TextBox txtnum;
+        private Button btnadd;
+        private TextBox txtA;
+        private TextBox txtB;
+        private Label lblA;
+        private Label lblB;
+        private Button btnvoltar;
+        private Button btnmostrar;
+        private Button btnreset;
+        private TextBox txtaddnum;
+        private Label lbladdnum;
+    }
+}
---
+++ Vetor_ex/Form4.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Vetor_ex
+{
+    partial class frmex3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblnum = new Label();
+            btnadd = new Button();
+            txtnum = new TextBox();
+            btnreset = new Button();
+            btnvoltar = new Button();
+            btnsearch = new Button();
+            txtsearch = new TextBox();
+            txtcubo = new TextBox();
+            lblsearch = new Label();
+            label1 = new Label();
+            lbln = new Label();
+            txtn = new TextBox();
+            btnlimpar = new Button();
+            txtaddnum = new TextBox();
+            lbladdnum = new Label();
+            SuspendLayout();
+            // 
+            // lblnum
+            // 
+            lblnum.AutoSize = true;
+            lblnum.Font = new Font("Segoe UI", 11.25F);
+            lblnum.Location = new Point(45, 64);
+            lblnum.Name = "lblnum";
+            lblnum.Size = new Size(0, 25);
+            lblnum.TabIndex = 17;
+            // 
+            // btnadd
+            // 
+            btnadd.Font = new Font("Segoe UI", 11.25F);
+            btnadd.Location = new Point(45, 167);
+            btnadd.Margin = new Padding(3, 4, 3, 4);
+            btnadd.Name = "btnadd";
+            btnadd.Size = new Size(165, 35);
+            btnadd.TabIndex = 16;
+            btnadd.Text = "Adicionar Número";
+            btnadd.UseVisualStyleBackColor = true;
+            btnadd.Click += btnadd_Click;
+            // 
+            // txtnum
+            // 
+            txtnum.Font = new Font("Segoe UI", 11.25F);
+            txtnum.Location = new Point(45, 111);
+            txtnum.Margin = new Padding(3, 4, 3, 4);
+            txtnum.Name = "txtnum";
+            txtnum.Size = new Size(165, 32);
+            txtnum.TabIndex = 15;
+            // 
+            // btnreset
+            // 
+            btnreset.Location = new Point(305, 364);
+            btnreset.Name = "btnreset";
+            btnreset.Size = new Size(91, 32);
+            btnreset.TabIndex = 21;
+            btnreset.Text = "Resetar";
+            btnreset.UseVisualStyleBackColor = true;
+            btnreset.Click += btnreset_Click;
+            // 
+            // btnvoltar
+            // 
+            btnvoltar.Location = new Point(409, 364);
+            btnvoltar.Name = "btnvoltar";
+            btnvoltar.Size = new Size(91, 32);
+            btnvoltar.TabIndex = 20;
+            btnvoltar.Text = "Voltar";
+            btnvoltar.UseVisualStyleBackColor = true;
+            btnvoltar.Click += btnvoltar_Click;
+            // 
+            // btnsearch
+            // 
+            btnsearch.Location = new Point(448, 80);
+            btnsearch.Name = "btnsearch";
+            btnsearch.Size = new Size(31, 27);
+            btnsearch.TabIndex = 22;
+            btnsearch.Text = "🔎";
+            btnsearch.UseVisualStyleBackColor = true;
+            btnsearch.Click += btnsearch_Click;
+            // 
+            // txtsearch
+            // 
+            txtsearch.Location = new Point(342, 80);
+            txtsearch.Name = "txtsearch";
+            txtsearch.Size = new Size(100, 32);
+            txtsearch.TabIndex = 23;
+            // 
+            // txtcubo
+            // 
+            txtcubo.Location = new Point(379, 141);
+            txtcubo.Name = "txtcubo";
+            txtcubo.ReadOnly = true;
+            txtcubo.Size = new Size(100, 32);
+            txtcubo.TabIndex = 24;
+            // 
+            // lblsearch
+            // 
+            lblsearch.AutoSize = true;
+            lblsearch.Location = new Point(289, 45);
+            lblsearch.Name = "lblsearch";
+            lblsearch.Size = new Size(250, 25);
+            lblsearch.TabIndex = 25;
+            lblsearch.Text = "Pesquise o cubo pelo índice:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(323, 144);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 25);
+            label1.TabIndex = 26;
+            label1.Text = "Cubo:";
+            // 
+            // lbln
+            // 
+            lbln.AutoSize = true;
+            lbln.Location = new Point(305, 205);
+            lbln.Name = "lbln";
+            lbln.Size = new Size(85, 25);
+            lbln.TabIndex = 27;
+            lbln.Text = "Número:";
+            // 
+            // txtn
+            // 
+            txtn.Location = new Point(379, 202);
+            txtn.Name = "txtn";
+            txtn.ReadOnly = true;
+            txtn.Size = new Size(100, 32);
+            txtn.TabIndex = 28;
+            // 
+            // btnlimpar
+            // 
+            btnlimpar.Location = new Point(395, 246);
+            btnlimpar.Name = "btnlimpar";
+            btnlimpar.Size = new Size(84, 37);
+            btnlimpar.TabIndex = 29;
+            btnlimpar.Text = "Limpar";
+            btnlimpar.UseVisualStyleBackColor = true;
+            btnlimpar.Click += btnlimpar_Click;
+            // 
+            // txtaddnum
+            // 
+            txtaddnum.Location = new Point(32, 364);
+            txtaddnum.Name = "txtaddnum";
+            txtaddnum.ReadOnly = true;
+            txtaddnum.Size = new Size(100, 32);
+            txtaddnum.TabIndex = 30;
+            // 
+            // lbladdnum
+            // 
+            lbladdnum.AutoSize = true;
+            lbladdnum.Location = new Point(32, 332);
+            lbladdnum.Name = "lbladdnum";
+            lbladdnum.Size = new Size(202, 25);
+            lbladdnum.TabIndex = 31;
+            lbladdnum.Text = "Números Adicionados:";
+            // 
+            // frmex3
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(540, 425);
+            ControlBox = false;
+            Controls.Add(lbladdnum);
+            Controls.Add(txtaddnum);
+            Controls.Add(btnlimpar);
+            Controls.Add(txtn);
+            Controls.Add(lbln);
+            Controls.Add(label1);
+            Controls.Add(lblsearch);
+            Controls.Add(txtcubo);
+            Controls.Add(txtsearch);
+            Controls.Add(btnsearch);
+            Controls.Add(btnreset);
+            Controls.Add(btnvoltar);
+            Controls.Add(lblnum);
+            Controls.Add(btnadd);
+            Controls.Add(txtnum);
+            Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "frmex3";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Exercício 3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblnum;
+        private Button btnadd;
+        private TextBox txtnum;
+        private Button btnreset;
+        private Button btnvoltar;
+        private Button btnsearch;
+        private TextBox txtsearch;
+        private TextBox txtcubo;
+        private Label lblsearch;
+        private Label label1;
+        private Label lbln;
+        private TextBox txtn;
+        private Button btnlimpar;
+        private TextBox txtaddnum;
+        private Label lbladdnum;
+    }
+}
